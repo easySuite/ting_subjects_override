a648d170be2b83df369037487c6d68cdf599f192
Alex <bozeak@gmail.com>
2018-09-13 17:02:27 +0300

    EASYOPAC-954 - Rewrite behavior of getSubjects method.

---
+++ lib/SO.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SO;
+
+use TingEntity;
+
+/**
+ * Class SubjectsOverrides.
+ *
+ * @package SO
+ */
+class SubjectsOverrides extends TingEntity {
+
+  /**
+   * SubjectsOverrides constructor.
+   *
+   * @param \TingEntity $entity
+   *   TingEntity object.
+   */
+  public function __construct(TingEntity $entity) {
+    parent::__construct();
+    $this->entity = $entity;
+  }
+
+  /**
+   * Get MARC-format subjects for ting_object.
+   *
+   * @return array|null
+   *   Return array of MARC format subjects.
+   */
+  public function getSubjects() {
+    $subjects = ting_get_object_marcxchange($this->entity->ding_entity_id);
+
+    if (empty($subjects)) {
+      return NULL;
+    }
+
+    $results = $subjects->getValue('667', '');
+
+    $items = [];
+    foreach ($results as $key => $item) {
+      // Omitting results with numeric indexes.
+      if (!is_numeric($key)) {
+        // If there are several sub-fields (arrays), we will need to loop them
+        // too.
+        if (is_array($item)) {
+          foreach ($item as $value) {
+            $items[] = $value;
+          }
+        }
+        else {
+          $items[] = $item;
+        }
+      }
+    }
+
+    return $this->entity->subjects = $items;
+  }
+
+}
